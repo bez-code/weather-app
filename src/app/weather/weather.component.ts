981e61c33cfe91b1ab7eb345c89c774dc931237f
bez <Behzad.p.ashrafi@gmail.com>
2023-11-26 22:18:13 +0300

    weather component

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { Weather } from '../weather';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent {
+
+  weather: Weather | undefined;
+
+  constructor (private weatherService: WeatherService){}
+
+  search (city: string){
+    this.weatherService.getWeather(city).subscribe(weather => this.weather =weather);
+  }
+
+}
