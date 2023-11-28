99d2cb6279db0a021caf51479a9f5b8c3de02895
bez <Behzad.p.ashrafi@gmail.com>
2023-11-28 21:47:37 +0300

    SnackBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+
+  constructor(private update: SwUpdate,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit(){
+    this.update.versionUpdates.pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      switchMap(() => this.snackBar.open('A new version is available!', 'Update now').afterDismissed()),
+      filter(result => result.dismissedByAction), map(() =>
+        this.update.activateUpdate().then(() => location.reload()))).subscribe();
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule} from '@angular/common/http';
-import { WeatherComponent } from './weather/weather.component'
-import {MatCardModule} from '@angular/material/card'
-import {MatIconModule} from '@angular/material/icon'
-import {MatInputModule} from '@angular/material/input'
+import { WeatherComponent } from './weather/weather.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +24,13 @@ import {MatInputModule} from '@angular/material/input'
     MatIconModule,
     MatInputModule,
     MatCardModule,
+    MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerImmediately'
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
